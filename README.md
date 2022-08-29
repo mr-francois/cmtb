39c0fde6fb015633aa5eaa9e5a3fbd55e12d7d85
mr-francois <mr.francois@gmx.de>
2022-08-29 16:23:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Create a confusion matrix in tensorboard
+
+Track training and validation scores of your model for every epoch.
+
+![](images/tb_train.png)
+
+# Example 
+
+See this [notebook]()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+ name='confusion_matrix_tensorboard',
+ version='0.0.1',
+ description='Confusion matrix in tensorboard',
+ url='https://github.com/mr-francois/confusion_matrix_tensorboard',
+ author_email='mr.francois@gmx.de',
+ install_requires=['tensorflow', 'numpy', 'sklearn', 'io', 'tempfile', 'matplotlib', 'os']
+)
