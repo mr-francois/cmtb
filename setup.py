c7fdd2d78d1d9df27355c51862ae0a84eb8897a9
mr-francois <mr.francois@gmx.de>
2022-08-29 17:34:46 +0200

    refactor

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
  name='cmtb',
  version='0.0.1',
  description='Confusion matrix in tensorboard',
- url='https://github.com/mr-francois/confusion_matrix_tensorboard',
+ url='https://github.com/mr-francois/cmtb',
  author_email='mr.francois@gmx.de',
  install_requires=['tensorflow', 'numpy', 'sklearn', 'matplotlib'],
  py_modules=[]
