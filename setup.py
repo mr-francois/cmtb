6864b98010f432283c950cb8265fc98560b3bb06
mr-francois <mr.francois@gmx.de>
2022-08-29 17:24:13 +0200

    setup config

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
- name='confusion_matrix_tensorboard',
+ name='cmtb',
  version='0.0.1',
  description='Confusion matrix in tensorboard',
  url='https://github.com/mr-francois/confusion_matrix_tensorboard',
