54c2542a9e8ddfdf952858ded9e3e0b11006eb34
mr-francois <mr.francois@gmx.de>
2022-09-01 15:18:56 +0200

    add README

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
  name='cmtb',
  version='0.0.1',
@@ -7,5 +12,7 @@ setup(
  url='https://github.com/mr-francois/cmtb',
  author_email='mr.francois@gmx.de',
  install_requires=['tensorflow', 'numpy', 'sklearn', 'matplotlib'],
+ long_description=long_description,
+ long_description_content_type='text/markdown',
  py_modules=[]
 )
